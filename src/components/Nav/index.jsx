9a0a78c15880ffbffd19c6c49907b912d2ed588f
phembarl <phembarl@yahoo.com>
2020-05-24 06:45:07 +0100

    toggle functionality

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,36 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Nav.css';
 
-const Nav = () => (
-  <nav className="visible navbar nav-in">
-    <div className="logo" />
+class Nav extends Component {
+  state = {
+    toggleActive: false,
+  }
 
-    <div className="toggle-btn visible nav-link">
-      <span />
-      <span />
-      <span />
-    </div>
+  toggleButtonHndler = () => {
+    this.setState({ toggleActive: !this.state.toggleActive });
+  }
 
-    <div className="nav-items nav-right">
-      <div className="options">
-        <div className="avi">
-          <img className="pishicon" src={localStorage.getItem('avatar')} alt="" />
+  render() {
+    return (
+      <nav className="visible navbar nav-in">
+        <div className="logo" />
+
+        <div className="toggle-btn visible nav-link" onClick={this.toggleButtonHndler}>
+          <span />
+          <span />
+          <span />
         </div>
-      </div>
 
-      <a href="login.html" className="user visible nav-link logout active">Vision</a>
-      <a href="login.html" className="user visible nav-link logout">Technology</a>
-      <a href="login.html" className="user visible nav-link logout">Experience it</a>
-      <a href="login.html" className="user visible nav-link logout">Sign in</a>
+        <div className="nav-items nav-right">
+          <div className="options">
+            <div className="avi">
+              <img className="pishicon" src={localStorage.getItem('avatar')} alt="" />
+            </div>
+          </div>
 
-      <div className="menu-container">
-        <ul className="user-menu">
-          <li><span className="user-menu-link">Log Out</span></li>
-        </ul>
-      </div>
-    </div>
+          <a href="login.html" className="user visible nav-link logout active">Vision</a>
+          <a href="login.html" className="user visible nav-link logout">Technology</a>
+          <a href="login.html" className="user visible nav-link logout">Experience it</a>
+          <a href="login.html" className="user visible nav-link logout">Sign in</a>
 
-  </nav>
-);
+          <div className={`menu-container ${this.state.toggleActive ? 'active' : ''}`}>
+            <ul className="user-menu">
+              <li><span className="user-menu-link">Vision</span></li>
+              <li><span className="user-menu-link">Technology</span></li>
+              <li><span className="user-menu-link">Experience it</span></li>
+              <li><span className="user-menu-link">Sign in</span></li>
+            </ul>
+          </div>
+        </div>
+
+      </nav>
+    );
+  }
+}
 
 export default Nav;
